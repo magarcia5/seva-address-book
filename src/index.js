ea7fa1710e7296a34c9632aa8ead0aa9d1de2a41
Mel Alrajhi <malrajhi@linkedin.com>
2019-02-20 18:45:18 -0800

    setting up webpack with react instead

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('imported');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React! Blah!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
